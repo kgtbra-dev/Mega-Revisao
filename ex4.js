206122836fceb6abf73fae7d2013305997d7d699
kgtbra-dev <kgtbra@gmail.com>
2026-09-12 22:49:39 -0300

    feat: adicionei o exercicio 4

---
+++ ex4.js
@@ -0,0 +1,12 @@
+let salario = 555;
+
+console.log(`Notas de R$ 200 : ${salario / 200}  .`);
+console.log(`Notas de R$ 100 : ${(salario % 200) / 100} .`);
+console.log(`Notas de R$ 50 : ${(salario % 100) / 50} .`);
+console.log(`Notas de R$ 20 : ${(salario % 50) / 20} .`);
+console.log(`Notas de R$ 10 : ${(salario % 20) / 10} .`);
+console.log(`Notas de R$ 5 : ${(salario % 10) / 5} .`);
+console.log(`Notas de R$ 2 : ${(salario % 5) / 2 } .`);
+console.log(`Notas de R$ 1 : ${(salario % 2) / 1} .`);
+
+
