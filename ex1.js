9b1036caccf679a694bb4fe09c1128ab6b30f5c5
kgtbra-dev <kgtbra@gmail.com>
2026-09-12 22:17:37 -0300

    Começando a lista do Eduardo

---
+++ ex1.js
@@ -0,0 +1,9 @@
+let num1 = 20;
+let num2 = 10;
+
+console.log(`A soma de ${num1} + ${num2} é: ${num1 + num2}`);
+console.log(`A subtração de ${num1} - ${num2} é: ${num1 - num2}`);
+console.log(`A multiplicação de ${num1} * ${num2} é: ${num1 * num2}`);
+console.log(`A divisão de ${num1} / ${num2} é: ${num1 / num2}`);
+console.log(`O resto da divisão de ${num1} por ${num2} é: ${num1 % num2}`);
+console.log(`A potência de ${num1} elevado a ${num2} é: ${num1 ** num2}`);
