12bc009b08f0a7d6568e17d4fb8ad4a6ac1bd351
kgtbra-dev <kgtbra@gmail.com>
2026-09-12 22:32:00 -0300

    feat: adicionei o exercicio 2

---
+++ ex2.js
@@ -0,0 +1,10 @@
+let tempo = 3670;
+let horas = tempo / 3600;
+let minutos = tempo / 60;
+let segundos = tempo % 60;
+
+console.log(`Você terá ${horas} horas.`);
+console.log(`Você terá ${minutos} minutos.`);
+console.log(`Você terá ${segundos} segundos.`);
+
+//???????? como, me diz como
