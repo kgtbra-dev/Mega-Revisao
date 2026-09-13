d34b0e27a3e01df5a35ebbade3f36c0adfede45c
kgtbra-dev <kgtbra@gmail.com>
2026-09-12 22:37:12 -0300

    feat: adicionei o exercicio 3

---
+++ ex3.js
@@ -0,0 +1,12 @@
+let salario = 3600;
+let horas = 60;
+
+let valor_hora = salario / 60;
+let valor_minuto =  salario / 3600;
+let valor_segundo = salario / 216000 ;
+
+console.log(`Você recebe R$ ${valor_hora}  por hora.`);
+console.log(`Você recebe R$ ${valor_minuto} por minuto.`);
+console.log(`Você recebe R$ ${valor_segundo} por segundo.`);
+
+
